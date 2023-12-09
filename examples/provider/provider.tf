3059a84c4fea9eda60ad5326421e39dfc8654e16
A3TechSlayer <kingslayeramouchi@gmail.com>
2023-12-09 23:27:58 +0100

    AIO commit

---
+++ examples/provider/provider.tf
@@ -0,0 +1,9 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# Configuration-based authentication
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+  host     = "http://localhost:9090"
+}
